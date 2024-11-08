029b33c4d0ac5d99204c2f340f425bc64e486264
Vincent Lav <vincent00lav@gmail.com>
2024-11-08 17:30:07 +0100

    Script Update

--- script.js
+++ script.js
@@ -33,10 +33,10 @@ document.addEventListener("DOMContentLoaded", function () {
       pseudoTooltip.style.border = "1px solid red";
     }
 
-    let phone = document.getElementById("phone");
-    let phoneTooltip = document
-      .getElementById("phone")
-      .parentElement.querySelector(".tooltip");
+    // let phone = document.getElementById("phone");
+    // let phoneTooltip = document
+    //   .getElementById("phone")
+    //   .parentElement.querySelector(".tooltip");
 
     let password = document.getElementById("password").value;
     let passwordConfirm = document.getElementById("passwordConfirm").value;
