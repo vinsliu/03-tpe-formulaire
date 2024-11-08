f05227adb2f8cdcdcc1768bbda91f21899fa3fa4
Vincent Lav <vincent00lav@gmail.com>
2024-11-08 17:26:27 +0100

    script mdp et pseudo

--- script.js
+++ script.js
@@ -18,6 +18,21 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
 
+    let pseudo = document.getElementById("pseudo").value;
+    let pseudoTooltip = document
+      .getElementById("pseudo")
+      .parentElement.querySelector(".tooltip");
+    if (pseudo.length < 4 || pseudo.length > 14) {
+      isValid = false;
+      pseudoTooltip.style.display = "block";
+      pseudoTooltip.style.display = "block";
+      pseudoTooltip.style.visibility = "visible";
+      pseudoTooltip.textContent =
+        "Le pseudo doit contenir entre 4 à 14 caractères !";
+      pseudoTooltip.style.color = "red";
+      pseudoTooltip.style.border = "1px solid red";
+    }
+
     let phone = document.getElementById("phone");
     let phoneTooltip = document
       .getElementById("phone")
@@ -26,6 +41,9 @@ document.addEventListener("DOMContentLoaded", function () {
     let password = document.getElementById("password").value;
     let passwordConfirm = document.getElementById("passwordConfirm").value;
     let passwordTooltip = document
+      .getElementById("password")
+      .parentElement.querySelector(".tooltip");
+    let passwordCTooltip = document
       .getElementById("passwordConfirm")
       .parentElement.querySelector(".tooltip");
     if (password !== passwordConfirm) {
@@ -35,6 +53,11 @@ document.addEventListener("DOMContentLoaded", function () {
       passwordTooltip.textContent = "Les mots de passe ne correspondent pas";
       passwordTooltip.style.color = "red";
       passwordTooltip.style.border = "1px solid red";
+      passwordCTooltip.style.display = "block";
+      passwordCTooltip.style.visibility = "visible";
+      passwordCTooltip.textContent = "Les mots de passe ne correspondent pas";
+      passwordCTooltip.style.color = "red";
+      passwordCTooltip.style.border = "1px solid red";
     }
 
     if (isValid) {
